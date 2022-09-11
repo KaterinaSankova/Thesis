7b90f7e75f9efd2f1dba1029504c6f8a9bbc52c1
Kittys777 <79199259+Kittys777@users.noreply.github.com>
2022-09-11 20:12:54 +0200

    Set up the repositary

--- TravelingSalesmanProblem/TravelingSalesmanProblem/Program.cs
+++ TravelingSalesmanProblem/TravelingSalesmanProblem/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,26 @@ namespace TravelingSalesmanProblem
     {
         static void Main(string[] args)
         {
+            string path = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), @"..\..\..\..\TestingData"));
+
+            Console.WriteLine($"Directory:\n{path}\nFiles:");
+
+            string[] files = Directory.GetFiles(path, "*.tsp");
+
+            foreach (var file in files)
+            {
+                Console.WriteLine(file);
+            }
+
+            using var reader = new StringReader(files[0]);
+            string? item;
+            do
+            {
+                item = reader.ReadLine();
+                Console.WriteLine(item);
+            } while (item != null);
+
+            Console.ReadKey();
         }
     }
 }
