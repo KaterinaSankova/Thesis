6d19bd0cf34a7a0c99d98a2322b536e44acfd9a3
Kittys777 <79199259+Kittys777@users.noreply.github.com>
2022-10-02 11:21:05 +0200

    New SetUp

---
+++ TravellingSalesmanProblem/TravellingSalesmanProblem/Node.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravellingSalesmanProblem
+{
+    public class Node
+    {
+        public double x;
+        public double y;
+
+        public Node(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public override string ToString()
+        {
+            return $"[{x}, {y}]";
+        }
+    }
+}
---
+++ TravellingSalesmanProblem/TravellingSalesmanProblem/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace TravellingSalesmanProblem
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string path = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), @"..\..\..\..\..\TestingData"));
+
+            string[] files = Directory.GetFiles(path, "*.tsp");
+
+            Stopwatch stopwatch = new Stopwatch();
+
+            Console.WriteLine(files[0]);
+
+            //stopwatch.Start();
+            //var matrix = new TSPLIBDeserializer(files[0]).DeserializeToEdges2();
+            //stopwatch.Stop();
+            //Console.WriteLine($"{stopwatch.Elapsed.Hours}H {stopwatch.Elapsed.Minutes}M {stopwatch.Elapsed.Seconds}S {stopwatch.Elapsed.Milliseconds}MS");
+
+            //stopwatch.Start();
+            //matrix = new TSPLIBDeserializer(files[0]).DeserializeToEdges3();
+            //stopwatch.Stop();
+            //Console.WriteLine($"{stopwatch.Elapsed.Hours}H {stopwatch.Elapsed.Minutes}M {stopwatch.Elapsed.Seconds}S {stopwatch.Elapsed.Milliseconds}MS");
+
+            //Console.WriteLine(files[1]);
+
+            //stopwatch.Start();
+            //matrix = new TSPLIBDeserializer(files[1]).DeserializeToEdges2();
+            //stopwatch.Stop();
+            //Console.WriteLine($"{stopwatch.Elapsed.Hours}H {stopwatch.Elapsed.Minutes}M {stopwatch.Elapsed.Seconds}S {stopwatch.Elapsed.Milliseconds}MS");
+
+            //stopwatch.Start();
+            //matrix = new TSPLIBDeserializer(files[1]).DeserializeToEdges3();
+            //stopwatch.Stop();
+            //Console.WriteLine($"{stopwatch.Elapsed.Hours}H {stopwatch.Elapsed.Minutes}M {stopwatch.Elapsed.Seconds}S {stopwatch.Elapsed.Milliseconds}MS");
+
+            ////Console.WriteLine(files[2]);
+
+            stopwatch.Start();
+            var matrix = new TSPLIBDeserializer(files[2]).DeserializeToEdges2();
+            stopwatch.Stop();
+            Console.WriteLine($"{stopwatch.Elapsed.Hours}H {stopwatch.Elapsed.Minutes}M {stopwatch.Elapsed.Seconds}S {stopwatch.Elapsed.Milliseconds}MS");
+
+            //for (int i = 0; i < matrix.Count; i++)
+            //{
+            //    for (int j = 0; j < matrix.Count; j++)
+            //    {
+            //        Console.Write($"{matrix[i][j]}\t" );
+            //    }
+            //    Console.Write($"\n");
+            //}
+
+            Console.ReadKey();
+        }
+    }
+}
