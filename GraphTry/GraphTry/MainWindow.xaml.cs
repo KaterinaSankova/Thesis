c372989b2b09c910d520e2377b895dabbd6492d6
Kittys777 <79199259+Kittys777@users.noreply.github.com>
2023-02-17 12:19:56 +0100

    Goin home

--- GraphTry/GraphTry/MainWindow.xaml.cs
+++ GraphTry/GraphTry/MainWindow.xaml.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TravellingSalesmanProblem.Algorithms;
 using TravellingSalesmanProblem.GraphStructures;
 
 namespace GraphTry
@@ -22,7 +23,7 @@ namespace GraphTry
     public partial class MainWindow : Window
     {
         private Line xAxisLine, yAxisLine;
-        private double xAxisStart = 100, yAxisStart = 100, interval = 0.5, lowestX = -1, lowestY = -5, highestX = 7, highestY = 9;
+        private double xAxisStart = 100, yAxisStart = 100, interval = 0.5, lowestX = -1, lowestY = -5, highestX = 5, highestY = 6;
         private Polyline chartPolyline;
 
         private Point origin;
@@ -33,49 +34,36 @@ namespace GraphTry
         {
             InitializeComponent();
 
-            values = new List<Node>()
+            Graph graph = new Graph( new List<Node>()
             {
-                //new Value(0,0),
-                //new Value(100,100),
-                //new Value(200,200),
-                //new Value(300,300),
-                //new Value(400,200),
-                //new Value(500,500),
-                //new Value(600,500),
-                //new Value(700,500),
-                //new Value(800,500),
-                //new Value(900,600),
-                //new Value(1000,200),
-                //new Value(1100,100),
-                //new Value(1200,400),
+                new Node(0, -1, -2),
+                new Node(1, -0.7, 0.3),
+                new Node(2, 2, 2.7),
+                new Node(3, 4, -2),
+                new Node(4, 4, 3)
 
-                //new Value(0,0),
-                //new Value(100,200),
-                //new Value(200,100),
-                //new Value(300,200),
-                //new Value(400,300),
-                //new Value(500,400),
-                //new Value(600,500),
-                //new Value(700,400),
-                //new Value(800,500),
-                //new Value(900,600),
-                //new Value(1000,300),
-                //new Value(1100,100),
-                //new Value(1200,400),
+            });
 
-                //new Node(0, -1, -2),
-                //new Node(1, -0.7, 0.3),
-                //new Node(2, 2, 2.7),
-                //new Node(3, 4, -2),
-                //new Node(4, 4, 3)
+            values = graph.nodes;
+            var bounds = graph.GetExtremeCoordinatesValues();
+            lowestX = bounds.MinX;
+            lowestY = bounds.MinY;
+            highestX = bounds.MaxX;
+            highestY = bounds.MaxY;
 
-                new Node(0, 100, 100),
-                new Node(1, 200, 100),
-                new Node(2, 100, 200),
-                new Node(3, 300, 500),
-                new Node(4, 400, 300)
+            Paint();
+            
 
-            };
+
+            var nit = new NiceInstanceTransformer();
+            graph = nit.TransformToNiceInstance(graph, 1);
+            bounds = graph.GetExtremeCoordinatesValues();
+            lowestX = bounds.MinX;
+            lowestY = bounds.MinY;
+            highestX = bounds.MaxX;
+            highestY = bounds.MaxY;
+
+            interval = 4;
 
             Paint();
 
@@ -99,42 +87,53 @@ namespace GraphTry
                     if (lowestY < 0) lowestYValue = 0 - lowestYValue;
 
                     int highestXValue = (int)Math.Ceiling(Math.Abs(highestX) / interval);
-                    if (lowestX < 0) highestXValue = 0 - highestXValue;
+                    if (highestX < 0) highestXValue = 0 - highestXValue;
 
                     int highestYValue = (int)Math.Ceiling(Math.Abs(highestY) / interval);
-                    if (lowestY < 0) highestYValue = 0 - highestYValue;
+                    if (highestY < 0) highestYValue = 0 - highestYValue;
 
                     int numberOfVerticalLines = Math.Abs(highestXValue - lowestXValue);
 
                     int numberOfHorizontalLines = Math.Abs(highestYValue - lowestYValue);
 
+                    (double X1, double X2, double Y1, double Y2) actualBounds = (xAxisStart, this.ActualWidth - xAxisStart, yAxisStart - 50, this.ActualHeight - yAxisStart);
+
+                    double trueHorizontalSpacing = Math.Abs(actualBounds.X1 - actualBounds.X2) / numberOfVerticalLines;
+                    double trueVerticalSpacing = Math.Abs(actualBounds.Y1 - actualBounds.Y2) / numberOfHorizontalLines;
+
                     // axis lines
                     xAxisLine = new Line()
                     {
-                        X1 = xAxisStart,
-                        Y1 = this.ActualHeight - yAxisStart + lowestYValue * interval,
-                        X2 = this.ActualWidth - xAxisStart,
-                        Y2 = this.ActualHeight - yAxisStart + lowestYValue * interval,
+                        X1 = actualBounds.X1,
+                        Y1 = this.ActualHeight - yAxisStart + lowestYValue * trueVerticalSpacing,
+                        X2 = actualBounds.X2,
+                        Y2 = this.ActualHeight - yAxisStart + lowestYValue * trueVerticalSpacing,
                         Stroke = Brushes.Black,
                         StrokeThickness = 5,
                     };
                     yAxisLine = new Line()
                     {
-                        X1 = xAxisStart - lowestXValue * interval,
-                        Y1 = yAxisStart - 50,
-                        X2 = xAxisStart - lowestXValue * interval,
-                        Y2 = this.ActualHeight - yAxisStart,
+                        X1 = xAxisStart - lowestXValue * trueHorizontalSpacing,
+                        Y1 = actualBounds.Y1,
+                        X2 = xAxisStart - lowestXValue * trueHorizontalSpacing,
+                        Y2 = actualBounds.Y2,
                         Stroke = Brushes.Black,
                         StrokeThickness = 5,
                     };
 
+                    origin = new Point(yAxisLine.X1, xAxisLine.Y1);
+
+                    var xTextBlock0 = new TextBlock() { Text = $"{0}", FontSize = 15 };
+                    chartCanvas.Children.Add(xTextBlock0);
+                    Canvas.SetLeft(xTextBlock0, origin.X + 6); // 6 = thickness os je 5, aby to bylo vidět
+                    Canvas.SetTop(xTextBlock0, origin.Y + 6);
+
                     // y axis lines
-                    double trueHorizontalSpacing = Math.Abs(xAxisLine.X1 - xAxisLine.X2) / numberOfVerticalLines;
 
                     double xValue = lowestXValue * interval;
                     double xPoint = xAxisLine.X1;
 
-                    while (xPoint < xAxisLine.X2)
+                    while (xValue <= highestXValue * interval)
                     {
                         var line = new Line()
                         {
@@ -159,18 +158,11 @@ namespace GraphTry
                         xValue += interval;
                     }
 
-
-                    var yTextBlock0 = new TextBlock() { Text = $"{0}" };
-                    chartCanvas.Children.Add(yTextBlock0);
-                    Canvas.SetLeft(yTextBlock0, origin.X - 20);
-                    Canvas.SetTop(yTextBlock0, origin.Y - 10);
-
                     // x axis lines
-                    double trueVerticalSpacing = Math.Abs(yAxisLine.Y1 - yAxisLine.Y2) / numberOfHorizontalLines;
 
                     var yValue = highestYValue * interval;
                     double yPoint = yAxisLine.Y1;
-                    while (yPoint < yAxisLine.Y2)
+                    while (yValue >= lowestYValue * interval)
                     {
                         var line = new Line()
                         {
@@ -199,36 +191,22 @@ namespace GraphTry
                     chartCanvas.Children.Add(xAxisLine);
                     chartCanvas.Children.Add(yAxisLine);
 
-                    origin = new Point(yAxisLine.X1, xAxisLine.Y1);
-
-                    var xTextBlock0 = new TextBlock() { Text = $"{0}", FontSize=15 };
-                    chartCanvas.Children.Add(xTextBlock0);
-                    Canvas.SetLeft(xTextBlock0, origin.X + 6); // 6 = thickness os je 5, aby to bylo vidět
-                    Canvas.SetTop(xTextBlock0, origin.Y + 6);
-
-
-                    // polyline
-                    chartPolyline = new Polyline()
-                    {
-                        Stroke = new SolidColorBrush(Color.FromRgb(68, 114, 196)),
-                        StrokeThickness = 10,
-                    };
-                    chartCanvas.Children.Add(chartPolyline);
 
                     // showing where are the connections points
+                    var ellipseRadius = 10;
                     foreach (var val in values)
                     {
                         Ellipse oEllipse = new Ellipse()
                         {
                             Fill = Brushes.Red,
-                            Width = 10,
-                            Height = 10,
+                            Width = ellipseRadius,
+                            Height = ellipseRadius,
                             Opacity = 1,
                         };
 
                         chartCanvas.Children.Add(oEllipse);
-                        Canvas.SetLeft(oEllipse, origin.X + val.x - 5);
-                        Canvas.SetTop(oEllipse, origin.Y - val.y - 5);
+                        Canvas.SetLeft(oEllipse, origin.X + (val.x / interval) * trueHorizontalSpacing - ellipseRadius/2);
+                        Canvas.SetTop(oEllipse, origin.Y - (val.y / interval) * trueVerticalSpacing - ellipseRadius/2);
                     }
 
                     foreach (var val1 in values)
@@ -237,12 +215,12 @@ namespace GraphTry
                         {
                             var line = new Line()
                             {
-                                X1 = origin.X + val1.x - 5,
-                                Y1 = origin.Y - val1.y - 5,
-                                X2 = origin.X + val2.x - 5,
-                                Y2 = origin.Y - val2.y - 5,
-                                Stroke = Brushes.AliceBlue,
-                                StrokeThickness = 5,
+                                X1 = origin.X + (val1.x / interval) * trueHorizontalSpacing,
+                                Y1 = origin.Y - (val1.y / interval) * trueVerticalSpacing,
+                                X2 = origin.X + (val2.x / interval) * trueHorizontalSpacing,
+                                Y2 = origin.Y - (val2.y / interval) * trueVerticalSpacing,
+                                Stroke = Brushes.Blue,
+                                StrokeThickness = 1,
                                 Opacity = 1,
                             };
 
@@ -254,8 +232,8 @@ namespace GraphTry
                     {
                         var xTextBlock1 = new TextBlock() { Text = $"{val}", FontSize = 15 };
                         chartCanvas.Children.Add(xTextBlock1);
-                        Canvas.SetLeft(xTextBlock1, origin.X + val.x);
-                        Canvas.SetTop(xTextBlock1, origin.Y - val.y);
+                        Canvas.SetLeft(xTextBlock1, origin.X + (val.x / interval) * trueHorizontalSpacing);
+                        Canvas.SetTop(xTextBlock1, origin.Y - (val.y / interval) * trueVerticalSpacing);
                     }
                 }
             }
